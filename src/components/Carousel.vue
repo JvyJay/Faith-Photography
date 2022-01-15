506b2df27a0f2f82e3555c9d66ae770ba947df23
Jadon Guzman <jjkolet25@tutanota.com>
2022-01-14 22:48:30 -0800

    fixed carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <b-carousel
+    id="carousel-fade"
+    style="text-shadow: 0px 0px 2px #000"
+    fade
+    indicators
+  >
+    <b-carousel-slide
+      v-bind:img-src="carousel1"
+      class="carousel-img"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      v-bind:img-src="carousel2"
+      class="carousel-img"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      v-bind:img-src="carousel3"
+      class="carousel-img"
+    ></b-carousel-slide>
+    <b-carousel-slide
+      v-bind:img-src="carousel4"
+      class="carousel-img"
+    ></b-carousel-slide>
+  </b-carousel>
+</div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.component("Carousel", {
+    props: {
+        carousel1: String,
+        carousel2: String,
+        carousel3: String,
+        carousel4: String
+    }
+})
+</script>
+
+<style scoped>
+.carousel-img {
+  height: 75vh !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,37 @@
 <template>
-  <div class="home">
-    <h1>Home Page</h1>
+  <div>
+    <Carousel 
+    v-bind:carousel1="carousel1"
+    v-bind:carousel2="carousel2"
+    v-bind:carousel3="carousel3"
+    v-bind:carousel4="carousel4" />
   </div>
 </template>
 
 <script>
+// Components
+import Carousel from '../components/Carousel.vue';
+// Carousel Images
+import carousel1 from "D:/Web Dev-Design/Portfolio-Projects/faith-photography/img/carousel01.jpg";
+import carousel2 from "D:/Web Dev-Design/Portfolio-Projects/faith-photography/img/carousel02.jpg";
+import carousel3 from "D:/Web Dev-Design/Portfolio-Projects/faith-photography/img/carousel03.jpg";
+import carousel4 from "D:/Web Dev-Design/Portfolio-Projects/faith-photography/img/carousel04.jpg";
+
 
 export default {
-  name: 'Home'
-}
+  name: 'Home',
+
+  components: {
+    Carousel
+  },
+
+  data() {
+    return {
+      carousel1: carousel1,
+      carousel2: carousel2,
+      carousel3: carousel3,
+      carousel4: carousel4
+      }
+    }
+  }
 </script>
