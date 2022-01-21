f70ddab2bd98762e115bd4e7b1c4f01b03639e3b
Jadon Guzman <jjkolet25@tutanota.com>
2022-01-20 21:15:26 -0800

    about card & page setup

---
+++ src/components/AboutCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <b-card no-body class="overflow-hidden mx-auto" style="max-width: 1000px;">
+    <b-row no-gutters>
+      <b-col>
+        <b-card-img :src="img" :alt="alt" class="rounded-1"></b-card-img>
+      </b-col>
+      <b-col>
+        <b-card-body title="Horizontal Card">
+          <b-card-text>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae corporis aperiam, natus saepe aliquid porro cumque harum officia vel qui voluptate commodi, sint aliquam! Esse cupiditate illo modi harum, molestiae, similique fuga suscipit quis in architecto odit doloremque sapiente qui. Ab eum architecto placeat eveniet eaque fugit saepe iure aliquam repellat dolorem, repellendus maxime quisquam rerum laboriosam facilis, ducimus quae corporis unde! Ducimus quae dolorem cupiditate dolor ipsum maxime possimus quibusdam eos amet optio, facilis assumenda aliquam, suscipit veniam.
+          </b-card-text>
+        </b-card-body>
+      </b-col>
+    </b-row>
+  </b-card>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+import img from "D:/Web Dev-Design/Portfolio-Projects/faith-photography/src/assets/about-photo.jpeg"
+
+export default Vue.component("AboutCard",{
+    name: "AboutCard",
+    data() {
+        return {
+            img: img,
+            alt: "About Image"
+        }
+    }
+})
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Me</h1>
+    <AboutCard />
+    <Footer />
   </div>
 </template>
+
+<script>
+// Components
+import AboutCard from "../components/AboutCard.vue"
+import Footer from "../components/Footer.vue";
+export default {
+  name: "About",
+  components: {
+    AboutCard,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+</style>
+
